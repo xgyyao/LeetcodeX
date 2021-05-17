48d5550898293602c311aeb66c98d6713dc5cbc1
xgyyao <34907486+xgyyao@users.noreply.github.com>
2021-05-18 00:46:15 +0800

    TwoSum算法解析

---
+++ 双指针/相向双指针/TwoSum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(0, len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+        return None
