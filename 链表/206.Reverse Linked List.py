04ea2076eebb94b4586ce014b20415c0cc583ff7
xgyyao <34907486+xgyyao@users.noreply.github.com>
2021-06-02 21:42:46 +0800

    反转链表代码

---
+++ 链表/206.Reverse Linked List.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pre = None
+        cur = head
+        while cur:
+            tmp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = tmp
+        return pre
