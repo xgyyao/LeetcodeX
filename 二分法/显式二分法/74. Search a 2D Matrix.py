a2ae2c66a256f942efd6ca42d08fd10ec8c2e5fb
xgyyao <34907486+xgyyao@users.noreply.github.com>
2021-06-30 21:03:57 +0800

    74.搜索二维矩阵

---
+++ 二分法/显式二分法/74. Search a 2D Matrix.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        len_row, len_col = len(matrix), len(matrix[0])
+        l,r = 0, len_row - 1
+        while(l<r):
+            mid = (l+r+1)//2
+            if matrix[mid][0] <= target:
+                l = mid
+            else:
+                r = mid - 1
+        row = l
+        l,r = 0, len_col - 1
+        while(l<r):
+            mid = (l+r+1)//2
+            if matrix[row][mid]<=target:
+                l = mid
+            else:
+                r = mid - 1
+        return target == matrix[row][l]
